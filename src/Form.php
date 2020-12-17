63283d7410d49d4f1e84aa474a8550d2f6372e44
Donald Wilcox <dowilcox@umich.edu>
2020-12-17 11:40:42 -0500

    start refactor of getting form and parsing information.

--- src/Form.php
+++ src/Form.php
@@ -2,13 +2,29 @@
 
 namespace UMFlint\GoogleFormsConverter;
 
+use GuzzleHttp\Client;
+
 class Form
 {
     /**
      * @var string
      */
-    protected $url;
+    protected string $url;
 
+    /**
+     * @var array
+     */
+    protected array $form;
+
+    /**
+     * @var string
+     */
+    protected string $fbzx;
+
+    /**
+     * Form constructor.
+     * @param string $url
+     */
     public function __construct(string $url)
     {
         $this->setUrl($url);
@@ -31,5 +47,58 @@ class Form
     public function getUrl(): string
     {
         return $this->url;
+    }
+
+    /**
+     * @return string
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    protected function getForm()
+    {
+        $client = new Client();
+        $response = $client->get($this->getUrl());
+
+        return (string)$response->getBody();
+    }
+
+    protected function parseForm(string $form): self
+    {
+
+
+        return $this;
+    }
+
+    /**
+     * @param string $form
+     * @return $this
+     * @throws \Exception
+     */
+    protected function parseFbzx(string $form): self
+    {
+        $matches = [];
+        preg_match('/name="fbzx" value="(.*)">/', $form, $matches);
+
+        if (empty($matches)) {
+            throw new \Exception('Invalid form! Could not find the fbzx field.');
+        }
+
+        print_r($matches[1]);
+        exit;
+
+        $this->fbzx = $matches[1];
+
+        return $this;
+    }
+
+    public function build(): string
+    {
+        $rawForm = $this->getForm();
+        $this->parseForm($rawForm);
+        $this->parseFbzx($rawForm);
+
+
+        $form = '';
+
+        return $form;
     }
 }
