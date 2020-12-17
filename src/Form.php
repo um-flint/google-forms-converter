8204d2f6678f1c16b39c3b78d156a7639002ba83
Donald Wilcox <dowilcox@umich.edu>
2020-12-17 14:30:48 -0500

    form parsing done.

--- src/Form.php
+++ src/Form.php
@@ -2,6 +2,8 @@
 
 namespace UMFlint\GoogleFormsConverter;
 
+use DOMDocument;
+use DOMXPath;
 use GuzzleHttp\Client;
 
 class Form
@@ -58,34 +60,48 @@ class Form
         $client = new Client();
         $response = $client->get($this->getUrl());
 
-        return (string)$response->getBody();
+        libxml_use_internal_errors(true);
+        $dom = new DOMDocument();
+        $dom->loadHTML((string)$response->getBody());
+
+        return $dom;
     }
 
-    protected function parseForm(string $form): self
+    /**
+     * @param DOMDocument $form
+     * @return $this
+     * @throws \Exception
+     */
+    protected function parseForm(DOMDocument $form): self
     {
+        $scripts = $form->getElementsByTagName('script');
+        foreach ($scripts as $script) {
+            if (strpos($script->textContent, 'var FB_PUBLIC_LOAD_DATA_') !== false) {
+                $text = str_replace(['var FB_PUBLIC_LOAD_DATA_ =', ';'], '', $script->textContent);
 
+                $this->form = json_decode($text, 0);
+                break;
+            }
+        }
 
         return $this;
     }
 
     /**
-     * @param string $form
+     * @param DOMDocument $form
      * @return $this
      * @throws \Exception
      */
-    protected function parseFbzx(string $form): self
+    protected function parseFbzx(DOMDocument $form): self
     {
-        $matches = [];
-        preg_match('/name="fbzx" value="(.*)">/', $form, $matches);
-
-        if (empty($matches)) {
+        $xp = new DOMXpath($form);
+        $nodes = $xp->query('//input[@name="fbzx"]');
+        if (!$nodes) {
             throw new \Exception('Invalid form! Could not find the fbzx field.');
         }
 
-        print_r($matches[1]);
-        exit;
-
-        $this->fbzx = $matches[1];
+        $node = $nodes->item(0);
+        $this->fbzx = $node->getAttribute('value');
 
         return $this;
     }
@@ -96,6 +112,8 @@ class Form
         $this->parseForm($rawForm);
         $this->parseFbzx($rawForm);
 
+        print_r($this->form);
+        exit;
 
         $form = '';
 
