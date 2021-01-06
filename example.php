498c5ebb07edcfd53881fc7f686153691b333a4e
Donald Wilcox <dowilcox@umich.edu>
2021-01-06 11:45:26 -0500

    Remove

--- example.php
+++ example.php
@@ -2,7 +2,6 @@
 
 include "vendor/autoload.php";
 
-//$form = new \UMFlint\GoogleFormsConverter\Form('https://docs.google.com/forms/d/e/1FAIpQLScuBOotKQTAle1GAg49BC4umx4nCBYHzh9SOiK-5P5eY-xU7A/viewform');
 $form = new \UMFlint\GoogleFormsConverter\Form('https://docs.google.com/forms/d/e/1FAIpQLSdls8ZHN0eT2UIKdC2FM730McPOxVPbY3WVJzsxtMQr5vKD1A/viewform');
 
 echo '<pre>';
